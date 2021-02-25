6ad80127438ff556c8c25af34ae8c4652485ba40
Sudhanshu Shekhar Ratnesh <r.ssr64@gmail.com>
2021-02-25 11:40:48 +0530

    add js file

---
+++ activity-form2.js
@@ -0,0 +1,10 @@
+jQuery(function () {
+  jQuery("#radio-000000650").on("click", function(){
+	alert("Testing jQuery");
+	jQuery("#0000006d_0").prop( "checked", false );
+  });
+  jQuery("#radio-0000006d0").on("click", function(){
+    jQuery("#00000065_0").prop( "checked", false );
+	alert("test jQuery");
+  });
+});  
