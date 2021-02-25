3526b5b80a96806b4c0085cc93f9a2304ffb15c5
Sudhanshu Shekhar Ratnesh <r.ssr64@gmail.com>
2021-02-25 20:05:34 +0530

    Update js file

--- activity-form2.js
+++ activity-form2.js
@@ -1,10 +1,10 @@
 jQuery(function () {
-  jQuery("#radio-000000650").on("click", function(){
+  jQuery("#radio-000000fb0").on("click", function(){
 	alert("Testing jQuery");
-	jQuery("#0000006d_0").prop( "checked", false );
+	jQuery("#00000101_0").prop( "checked", false );
   });
-  jQuery("#radio-0000006d0").on("click", function(){
-    jQuery("#00000065_0").prop( "checked", false );
+  jQuery("#radio-000001010").on("click", function(){
+    jQuery("#000000fb_0").prop( "checked", false );
 	alert("test jQuery");
   });
-});  
+});
